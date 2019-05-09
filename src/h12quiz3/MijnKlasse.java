b77ed9d89b6784a59ef242cc653a25c64a7aaf7b
blacksoijo2 <47447116+blacksoijo2@users.noreply.github.com>
2019-05-09 13:37:27 +0200

    h12quiz3

---
+++ src/h12quiz3/MijnKlasse.java
@@ -0,0 +1,18 @@
+package h12quiz3;
+//h12quiz3 gemaakt door: Zinho.
+public class MijnKlasse extends SuperKlasse
+{
+  int x = 3;
+  String methodeA(String s, int i)
+  {
+    String str = s + ", " + i;
+    super.x ++;
+    return str;
+  }
+  public static void main(String[] args)
+  {
+    MijnKlasse mk = new MijnKlasse();
+    System.out.print(mk.methodeA(23, "Emma"));
+  }
+}
+//opdracht: "super.x ++;" toegevoegd, error: superklasse.k can not be assigned. 
---
+++ src/h12quiz3/SuperKlasse.java
@@ -0,0 +1,11 @@
+package h12quiz3;
+//h12quiz3 gemaakt door: Zinho.
+public class SuperKlasse {
+
+  final int x = 4;
+  final String methodeA(int i, String s)
+  {
+    String str = i + ", " + s;
+    return str;
+  }
+}
