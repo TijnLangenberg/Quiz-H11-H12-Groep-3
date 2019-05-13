fbf27ddea902ca1a0bf1a0864aed23cd5f56bd16
blacksoijo2 <47447116+blacksoijo2@users.noreply.github.com>
2019-05-13 14:09:42 +0200

    code bijgewerkt

---
+++ src/h12quiz2/MijnKlasse.java
@@ -0,0 +1,21 @@
+package h12quiz2;
+//h12quiz2 gemaakt door: Tijn.
+public class MijnKlasse extends Superklasse
+{
+	int z = 5;
+	
+	void mijnMethode(int x)
+	{
+		System.out.print("X");
+	}
+	public static void main (String[] args)
+	{
+		MijnKlasse mk = new MijnKlasse();
+	}
+	//opdracht
+	void methodeB(int x)
+	{
+		System.out.print("X");
+		return;
+	}
+}
---
+++ src/h12quiz4/Superklasse.java
@@ -0,0 +1,11 @@
+package h12quiz4;
+//h12quiz4 gemaakt door: Tijn.
+public class Superklasse {
+
+	final void methodeA(int x, double y)
+	{
+		double z = x * y;
+		System.out.print(z);
+	}
+}
+
