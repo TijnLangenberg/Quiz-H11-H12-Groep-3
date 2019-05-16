fe13211ef8c92193f01e933d93b364e26a9f0537
blacksoijo2 <47447116+blacksoijo2@users.noreply.github.com>
2019-05-16 12:48:55 +0200

    fahd zijn code

---
+++ src/Hoofdstuk11Quizzen/h11quiz2/Subklasse.java
@@ -0,0 +1,12 @@
+public class Subklasse extends Superklasse{
+//h11quiz2 gemaakt door: Fahd.
+  Subklasse(int y)
+  {
+    x += y;
+  }
+  public static void main(String[] args)
+  {
+    Subklasse sk = new Subklasse(4);
+    System.out.print(sk.x);
+  }
+}
---
+++ src/Hoofdstuk11Quizzen/h11quiz2/Superklasse.java
@@ -0,0 +1,8 @@
+public class Superklasse {
+//h11quiz2 gemaakt door: Fahd.
+	  protected int x = 1;
+	  Superklasse()
+	  {
+	    x += 2;
+	  }
+	}
